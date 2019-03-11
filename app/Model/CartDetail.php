8ec1666fefe8b65e042dd672cbe23f80c5955075
khaipv <43499160+khaipv@users.noreply.github.com>
2019-03-11 22:37:08 +0700

    Update CartDetail.php

--- app/Model/CartDetail.php
+++ app/Model/CartDetail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartDetail extends Model
 {
-    protected $table ="chitietgiohang";
+    protected $table ="chi_tiet_gio_hang";
     public $timestamps = false;
     protected $fillable = ['so_luong','san_pham_id','gio_hang_id'];
 }
