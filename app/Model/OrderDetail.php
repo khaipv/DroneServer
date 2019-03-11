e126f5e044401a6390868b57b1a568fdc5b6bb9d
khaipv <43499160+khaipv@users.noreply.github.com>
2019-03-11 22:38:08 +0700

    Update OrderDetail.php

--- app/Model/OrderDetail.php
+++ app/Model/OrderDetail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    protected $table ="chitietdonhang";
+    protected $table ="chi_tiet_don_hang";
     public $timestamps = false;
     protected $fillable = ['so_luong','don_gia','san_pham_id','don_hang_id'];
 }
