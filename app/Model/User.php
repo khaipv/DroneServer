8a177421f8062596fffd4e739cd32bf3d32db18b
khaipv <43499160+khaipv@users.noreply.github.com>
2019-03-11 22:39:21 +0700

    Update User.php

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    protected $table ="nguoidung";
+    protected $table ="nguoi_dung";
     public $timestamps = false;
     protected $fillable = ['ho_ten','email','mat_khau','so_dien_thoai','dia_chi','vai_tro_id'];
 }
